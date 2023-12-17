1357577b63f9bd164c9c542c1100ed4e1adf4861
Xarlos <57622136+Xarlos89@users.noreply.github.com>
2023-12-17 18:07:39 +0100

    grab token from arg OR .env

--- src/main.py
+++ src/main.py
@@ -1,32 +1,52 @@
 import os
+import sys
 import discord
 from discord.ext import commands
 
 from logger import *
 
 
-ZorakV2 = os.getenv("TOKEN")
-prefix = os.getenv("PREFIX")
-
 intents = discord.Intents.all()
 intents.message_content = True
+bot = commands.Bot(command_prefix=os.getenv("PREFIX"), intents=intents)
 
-bot = commands.Bot(command_prefix=prefix, intents=intents)
 
-
-async def load_cogs(robot):
+async def load_cogs(robot: commands.Bot) -> None:
     """
     Loads the directories under the /cogs/ folder,
     then digs through those directories and loads the cogs.
+
+    We do not load files starting with _ and the templates folder.
     """
     log_info("Loading Cogs...")
     for directory in os.listdir("./cogs"):
-        if not directory.startswith("_"):  # Makes sure __innit.py__ doesnt get called
+        if not directory.startswith("_") and directory != "templates":
             for file in os.listdir(f"./cogs/{directory}"):
                 if file.endswith('.py') and not file.startswith("_"):
-                    log_info(f"Loading Cog: \\{directory}\\{file}")
+                    log_debug(f"Loading Cog: \\{directory}\\{file}")
                     await robot.load_extension(f"cogs.{directory}.{file[:-3]}")
     log_info(" - Success.")
+
+
+def load_key_and_run():
+    """
+    Loads the bot key as the first arg when running the bot OR from an env variable.
+    For example:
+        "python __main__.py BOT_TOKEN_HERE"
+    """
+    if len(sys.argv) > 1:  # Check args for the token first
+        token = sys.argv[1].replace('TOKEN=','')
+        log_debug('Loading Token from arg.')
+        bot.run(token)
+
+    elif os.environ['TOKEN'] is not None:  # if not in args, check the env vars
+        log_debug('Loading Token from environment variable.')
+        bot.run(os.environ['TOKEN'])
+
+    else:
+        log_critical('You must include a bot token...')
+        log_critical("TOKEN must be in the .env file")
+        log_critical('OR you must run the bot using: "python __main__.py TOKEN=YOUR_DISCORD_TOKEN"')
 
 
 @bot.event
@@ -34,7 +54,7 @@ async def setup_hook() -> None:
     """
     The setup_hook executes before the bot logs in.
     """
-    log_info("Executing set up hook...")
+    log_debug("Executing set up hook...")
     await load_cogs(bot)
 
 
@@ -43,8 +63,8 @@ async def on_ready() -> None:
     """
     The on_ready is executed AFTER the bot logs in.
     """
-    log_info("Executing on_ready event.")
+    log_debug("Executing on_ready event.")
     log_info(f'Logged in as {bot.user.name} - ({bot.user.id})')
 
 
-bot.run(ZorakV2)
+load_key_and_run()
